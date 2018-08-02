57e212863e314951e8c9fdb29615a718967765de
denis <denis.silva@sunsetcom.com.br>
2018-08-02 18:15:49 -0300

    finalizado

--- CursoVue-parte1-alura/alurapic/src/main.js
+++ CursoVue-parte1-alura/alurapic/src/main.js
@@ -2,13 +2,19 @@ import Vue from 'vue'
 import VueResource from 'vue-resource';
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VeeValidate, { Validator } from 'vee-validate';
+import msg from './pt_BR'
 import { routes } from './routes'
 import './directives/Transform'
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
+Vue.use(VeeValidate);
+Validator.localize('pt_BR', msg);
 
-const router = new VueRouter({ 
+Vue.http.options.root = 'http://localhost:3000'
+
+const router = new VueRouter({
   routes,
   mode: 'history'
  })
